c5178dbe9eb037a1c5b087a94de04e4f684beb27
Konstantin Shulga <mrkonstantinsh@gmail.com>
2021-03-22 10:46:21 +0300

    chore(lab-1): add project files

---
+++ Lab_1/Lab_1/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace Lab_1
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
